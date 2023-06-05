ca0e231cb4ee187bb467137d6a5bfcc15d8fc4db
Radoslav Irha <radoslav.irha@gmail.com>
2023-06-05 21:43:44 +0200

    feat: :sparkles: new /rain/image endpoint

--- laskakit-data-feeder/lib/routes/rain.ts
+++ laskakit-data-feeder/lib/routes/rain.ts
@@ -1,5 +1,5 @@
 import Controller from '../handlers/rain';
-import { IRouteGetRainRefs, IRouteGetRainValidation } from '../types/routes/rain';
+import { IRouteGetRainImageRefs, IRouteGetRainImageValidation, IRouteGetRainRefs, IRouteGetRainValidation } from '../types/routes/rain';
 import { HapiServerRoute } from '../types/server/route';
 import { getRainQueryValidator } from '../validators/routes/rain';
 
@@ -9,7 +9,7 @@ const rain: HapiServerRoute<IRouteGetRainRefs, IRouteGetRainValidation> = {
   handler: Controller.rain,
   options: {
     auth: false,
-    description: 'Rain',
+    description: 'Send rain data to LaskaKit',
     notes: [],
     plugins: {
       'hapi-swagger': {
@@ -26,6 +26,31 @@ const rain: HapiServerRoute<IRouteGetRainRefs, IRouteGetRainValidation> = {
   }
 };
 
+const rainImage: HapiServerRoute<IRouteGetRainImageRefs, IRouteGetRainImageValidation> = {
+  method: 'GET',
+  path: `/rain/image`,
+  handler: Controller.rainImage,
+  options: {
+    auth: false,
+    description: 'Returns radar and current conditions image',
+    notes: [],
+    plugins: {
+      'hapi-swagger': {
+        responses: {
+          200: { description: 'Radar and current conditions image', content: { 'image/png': { schema: { type: 'string', format: 'binary' } } } },
+          400: { description: 'Bad Request' }
+        },
+        produces: [ 'application/json', 'image/png' ]
+      }
+    },
+    tags: [ 'api' ],
+    validate: {
+      query: getRainQueryValidator
+    }
+  }
+};
+
 export default [
-  rain
+  rain,
+  rainImage
 ];
